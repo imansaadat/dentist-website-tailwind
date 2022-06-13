f7f02772108d56b6ca8b9798a0efac938197a6ef
ImanSaadat <e.saadat72@gmail.com>
2022-06-13 19:47:57 +0430

    navbar and js updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
-const navList = document.querySelector("#navList");
+const menu = document.querySelector("#menu");
 const hamMenu = document.querySelector("#hamMenu");
 const header = document.querySelector("#header");
 // hamMenu
-hamMenu.addEventListener("click", () => {
-  if(navList.classList.contains('left-[-100%]')){
-    navList.classList.remove("left-[-100%]")
-    navList.classList.add("left-0")
-  }
-  else{
-    navList.classList.add("left-[-100%]")
-    navList.classList.remove("left-0")
-  }
-});
+hamMenu.addEventListener('click', () => {
+  menu.classList.toggle('left-0')
+  menu.classList.toggle('left-[-100%]')
+  hamMenu.classList.toggle('fa-times')
+})
 
 // bg header
-window.addEventListener('scroll',()=>{
-  if(window.scrollY > 0){
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
     header.classList.add('bg-white')
-    header.classList.remove('border-b-2')
-  }else{
+    menu.classList.remove('left-0')
+    menu.classList.add('left-[-100%]')
+    hamMenu.classList.remove('fa-times')
+  } else {
     header.classList.remove('bg-white')
-    header.classList.add('border-b-2')
   }
 })
 
@@ -28,8 +24,8 @@ window.addEventListener('scroll',()=>{
 var swiper = new Swiper(".clients", {
   slidesPerView: 1,
   spaceBetween: 10,
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
